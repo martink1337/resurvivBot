334420ebf7295ae7dd2c01a0eb565b67bb2098be
veldreth <martin.kostadinov1337@gmail.com>
2025-06-05 21:12:55 +0300

    Update sticky.py

--- cogs/sticky.py
+++ cogs/sticky.py
@@ -1,39 +1,47 @@
 import discord
 from discord.ext import commands
+from config import config  # Импортираме конфигурацията
 
 class Sticky(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        # Ще пазим за всеки канал sticky съобщението (текст и съобщение обект)
         self.sticky = {}  # {channel_id: {"text": str, "message": discord.Message}}
+        self.staff_roles = config.get("staff_roles", [])
 
-    # ot tuk
     @commands.Cog.listener()
     async def on_ready(self):
         print("Sticky Cog Loaded")
-    # do tuk
 
     @commands.command()
     async def sticky(self, ctx, *, message: str):
         """Активира sticky съобщение в канала"""
+
+        user_roles = [r.name.lower() for r in ctx.author.roles]
+        if not any(role in user_roles for role in self.staff_roles):
+            await ctx.send("Нямаш нужните права да използваш тази команда.")
+            return
+
         channel_id = ctx.channel.id
 
-        # Ако има старо sticky съобщение в канала - изтриваме го
         if channel_id in self.sticky:
             try:
                 await self.sticky[channel_id]["message"].delete()
             except:
                 pass
 
-        # Пращаме новото sticky съобщение
         sticky_msg = await ctx.send(f"**Sticky:** {message}")
         self.sticky[channel_id] = {"text": message, "message": sticky_msg}
-
-        await ctx.send(f"Sticky съобщението е настроено.")
+        await ctx.send("Sticky съобщението е настроено.")
 
     @commands.command()
     async def unsticky(self, ctx):
         """Деактивира sticky съобщението в този канал"""
+
+        user_roles = [r.name.lower() for r in ctx.author.roles]
+        if not any(role in user_roles for role in self.staff_roles):
+            await ctx.send("Нямаш нужните права да използваш тази команда.")
+            return
+
         channel_id = ctx.channel.id
         if channel_id in self.sticky:
             try:
@@ -47,21 +55,17 @@ class Sticky(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Пропускаме съобщения от бота
         if message.author.bot:
             return
 
         channel_id = message.channel.id
         if channel_id in self.sticky:
-            # Изтриваме старото sticky съобщение
             try:
                 await self.sticky[channel_id]["message"].delete()
             except:
                 pass
 
-            # Пращаме отново sticky съобщението под новото съобщение
             sticky_msg = await message.channel.send(f"**Sticky:** {self.sticky[channel_id]['text']}")
-            # Обновяваме референцията
             self.sticky[channel_id]["message"] = sticky_msg
 
 async def setup(bot):
